fc641800d96bee7de353992a36abe8bc49aafc82
Tales313 <tales.11.98@gmail.com>
2021-12-24 11:36:52 -0300

    # 0.1.0-SNAPSHOT # > Implementação do alterar da conta repositorio

--- src/main/java/conta/servicos/repositorio/ContaRepositorioImp.java
+++ src/main/java/conta/servicos/repositorio/ContaRepositorioImp.java
@@ -5,6 +5,7 @@ import conta.sistema.dominio.modelo.NegocioException;
 import conta.sistema.porta.ContaRepositorio;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -44,9 +45,21 @@ public class ContaRepositorioImp implements ContaRepositorio {
         }
     }
 
+    @Transactional
     @Override
     public void alterar(Conta conta) {
+        if (isNull(conta))
+            throw new NegocioException("Conta é obrigatória.");
 
+        var sql = "update conta set saldo = ?, correntista = ? where numero = ?";
+        var pm = new Object[]{conta.getSaldo(), conta.getCorrentista(), conta.getNumero()};
+
+        try {
+            jdbc.update(sql, pm);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new NegocioException(ERRO);
+        }
     }
 
 }
