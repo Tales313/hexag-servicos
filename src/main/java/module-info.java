686e08e59bc5abbfeeb64eae10bb9fb2871c1eee
Tales313 <tales.11.98@gmail.com>
2021-12-24 11:09:20 -0300

    # 0.0.4-SNAPSHOT # > Configuração de módulo

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,18 @@
 module conta.servicos {
 
+    // usar conta sistema
+    requires conta.sistema;
 
+    // usar spring
+    requires javax.inject;
+    requires spring.tx;
+    requires spring.core;
+    requires spring.beans;
+    requires spring.context;
+    requires java.sql;
+    requires spring.jdbc;
+
+    // abre repositorio
+    opens conta.servicos.repositorio;
 
 }
