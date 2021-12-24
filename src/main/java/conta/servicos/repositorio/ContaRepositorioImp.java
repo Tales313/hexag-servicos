2c578f5e42a4bfac5d2a962b9fa7d9ae9d6c4903
Tales313 <tales.11.98@gmail.com>
2021-12-24 11:27:55 -0300

    # 0.0.5-SNAPSHOT # > Iniciando implementaão da ContaRepositorioImp

--- src/main/java/conta/servicos/repositorio/ContaRepositorioImp.java
+++ src/main/java/conta/servicos/repositorio/ContaRepositorioImp.java
@@ -1,4 +1,52 @@
 package conta.servicos.repositorio;
 
-public class ContaRepositorioImp {
+import conta.sistema.dominio.modelo.Conta;
+import conta.sistema.dominio.modelo.NegocioException;
+import conta.sistema.porta.ContaRepositorio;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import static java.util.Objects.isNull;
+
+@Named
+public class ContaRepositorioImp implements ContaRepositorio {
+
+    private static final String ERRO = "Erro inesperado de acesso ao banco de dados. Entre em contato com o administrador";
+    private JdbcTemplate jdbc;
+
+    @Inject
+    public ContaRepositorioImp(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    @Override
+    public Conta get(Integer numero) {
+        if(isNull(numero))
+            return null;
+
+        var sql = "select * from conta where numero = ?";
+        var pm = new Object[]{numero};
+        RowMapper<Conta> orm = (rs, nm) ->
+                new Conta(rs.getInt(1), rs.getBigDecimal(2), rs.getString(3));
+
+        try {
+            var lista = jdbc.query(sql, pm, orm);
+            if(lista.isEmpty())
+                return null;
+            else
+                return lista.get(0);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new NegocioException(ERRO);
+        }
+    }
+
+    @Override
+    public void alterar(Conta conta) {
+
+    }
+
 }
